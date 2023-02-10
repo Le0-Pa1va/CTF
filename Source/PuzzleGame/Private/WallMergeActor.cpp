25a0f2f234997fb52ebeed344d2f931f32dabcb7
Leonardo Paiva <102998015+Le0-Pa1va@users.noreply.github.com>
2023-02-10 17:17:25 -0300

    Updating wallmerge pawn to try to use the new logic

--- Source/PuzzleGame/Private/WallMergeActor.cpp
+++ Source/PuzzleGame/Private/WallMergeActor.cpp
@@ -11,16 +11,11 @@ AWallMergeActor::AWallMergeActor()
 
 	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
 	CharacterDecal = CreateDefaultSubobject<UDecalComponent>(TEXT("Decal"));
-	LineTracePoint = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
 	CharCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 
 	// Remember to set the decal size on the blueprint (i think the  size 30, 30, 30)
 
 	RootComponent = BoxCollision;
-	CharacterDecal->SetupAttachment(RootComponent);
-	LineTracePoint->SetupAttachment(CharacterDecal);
-	CharCamera->SetupAttachment(CharacterDecal);
-
 }
 
 // Called when the game starts or when spawned
